682fb00cbc39874303ac3a8734ef7edc747a58cb
Joseph Simone <joseph.simone@fpl.com>
2018-11-08 09:26:03 -0500

    added linting

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import numeral from 'numeral';
 import './index.css'
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+//debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
