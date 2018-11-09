c0ee8d90cc53429f81d8ac869c9ab05c2c34507f
Joseph Simone <joseph.simone@fpl.com>
2018-11-09 15:38:54 -0500

    make jsf call asynch using a promise

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -10,14 +10,19 @@ import jsf from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
+
+// faker and chance are no longer include in json-schema-faker 0.5.x and above, so must register as below
 jsf.extend('faker', () => require('faker'));
+jsf.extend('chance', () => require('chance'));
 
-const json = JSON.stringify(jsf.generate(schema));
 
-fs.writeFile("./src/api/db.json", json, function (err) {
-  if (err) {
-    return console.log(chalk.red(err));
-  } else {
-    console.log(chalk.green("Mock data generated."));
-  }
+// use the async-version (preferred way)
+jsf.resolve(schema).then(json => {
+  fs.writeFile("./src/api/db.json", JSON.stringify(json), function (err) {
+    if (err) {
+      return console.log(chalk.red(err));
+    } else {
+      console.log(chalk.green("Mock data generated."));
+    }
+  });
 });
