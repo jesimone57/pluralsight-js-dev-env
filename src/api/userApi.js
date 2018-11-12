8a5a85c51a9cc08e9fbf9158e5a565d1448c8a4d
Joseph Simone <joseph.simone@fpl.com>
2018-11-12 09:54:57 -0500

    delete user link works now against mock data

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,12 +1,24 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
 
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call this delete because it is a reserved word
+function del(url) {
+  const request = new Request(baseUrl + url, {method: 'DELETE'});
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
